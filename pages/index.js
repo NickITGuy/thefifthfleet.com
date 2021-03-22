23cd4bfab6efbe249c5a00f869c2f9ac8865c0b3
Nick Davis <nick@nicktheitguy.com>
2021-03-22 13:27:14 +1030

    Make it good?

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,19 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="head-content">
-      <h1>The Fifth Fleet - Gaming Community</h1>
-      <video src="/video.mkv" autoPlay muted></video>
-      <button>Discord</button>
+    <header class="v-header container">
+    <div class="fullscreen-video-wrap">
+      <video src="/video.mkv" autoplay muted loop></video>
     </div>
+    <div class="header-overlay"></div>
+    <div class="header-content">
+      <h1>The Fifth Fleet</h1>
+      <p>
+        The Fifth Fleet is a world wide gaming community! We play any games
+        aslong as they are with our fellow friends and comrades.
+      </p>
+      <a href="https://discord.gg/xWtNFVR" class="btn">Discord</a>
+    </div>
+  </header>
     )
 }
